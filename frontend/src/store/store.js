df777e3a61b216ad8647477e934ce470eb811ace
Yong Kim <yhkim4573@gmail.com>
2022-12-18 23:28:27 -0500

    twitter-mern

---
+++ frontend/src/store/store.js
@@ -0,0 +1,27 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
+import tweetsReducer from "./tweets";
+import errorsReducer from "./errors";
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  tweets: tweetsReducer,
+  errors: errorsReducer,
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
